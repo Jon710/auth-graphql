04ef3fc34b7fc8aee0348e0bd84439171efb6f91
João Luis Moraes <jonmoraesnl@gmail.com>
2021-04-22 19:15:30 -0300

    refresh token

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,8 +1,10 @@
-import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware } from 'type-graphql';
 import {hash, compare} from 'bcryptjs';
 import { User } from './entity/User';
 import { MyContext } from './MyContext';
 import { createAccessToken, createRefreshToken } from './auth';
+import { isAuth } from './isAuth';
+import { sendRefreshToken } from './sendRefreshToken';
 
 @ObjectType()
 class LoginResponse {
@@ -15,6 +17,13 @@ export class UserResolver {
     @Query(() => String)
     hello() {
         return 'hi'
+    }
+    
+    @Query(() => String)
+    @UseMiddleware(isAuth)
+    bye(@Ctx() { payload }: MyContext) {
+        console.log(payload)
+        return `your user id is: ${payload!.userId}`
     }
 
     @Query(() => [User])
@@ -42,10 +51,8 @@ export class UserResolver {
         }
 
         // login successful
-        res.cookie('jid', createRefreshToken(user),
-        {
-            httpOnly: true
-        })
+        sendRefreshToken(res, createRefreshToken(user));
+
        
         return {
             accessToken: createAccessToken(user)
