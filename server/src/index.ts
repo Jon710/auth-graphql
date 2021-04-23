64afea279c64565a87f09eaa9ff0517b23fa6f59
João Luis Moraes <jonmoraesnl@gmail.com>
2021-04-22 23:29:35 -0300

    config cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,12 +7,17 @@ import { UserResolver } from "./UserResolver";
 import { createConnection } from "typeorm";
 import cookieParser from 'cookie-parser';
 import { verify } from 'jsonwebtoken';
+import cors from 'cors';
 import { User } from './entity/User';
 import { createAccessToken, createRefreshToken } from './auth';
 import { sendRefreshToken } from './sendRefreshToken';
 
 (async () => {
     const app = express();
+    app.use(cors({
+        origin: 'http://localhost:3001',
+        credentials: true,
+    }))
     app.use(cookieParser());
 
     app.get('/', (_req, res) => res.send('Hello'));
@@ -57,27 +62,9 @@ import { sendRefreshToken } from './sendRefreshToken';
         context: ({ req, res }) => ({ req, res })
     })
 
-    apolloServer.applyMiddleware({ app })
+    apolloServer.applyMiddleware({ app, cors: false })
 
     app.listen(4000, () => {
         console.log('express server started!');
     })
 })()
-
-// createConnection().then(async connection => {
-
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     await connection.manager.save(user);
-//     console.log("Saved a new user with id: " + user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await connection.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log("Here you can setup and run express/koa/any other framework.");
-
-// }).catch(error => console.log(error));
